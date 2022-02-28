d6c2497a559574c4cd2ac7003c82353ee1c1fb0a
Инна Кляхина <inna.galkina95@ya.ru>
2022-03-01 01:10:27 +0300

    Выполненный flake8

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,3 @@
-from enum import unique
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -9,7 +8,7 @@ class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-    
+
     def __str__(self) -> str:
         return self.title
 
@@ -29,5 +28,3 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
-
-# Create your models here.
